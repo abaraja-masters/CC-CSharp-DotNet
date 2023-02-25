433a89278ab64fe837c1dafd28885b049e02f861
Anthony Baraja <atbaraja@gmail.com>
2023-02-24 21:41:39 -0800

    add ascii art project

---
+++ ConsoleCreatures/Program.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /*
+        We will be printing an ascii art to the console!
+      */
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");  
+      Console.WriteLine("| O |");  
+      Console.WriteLine("|   |");  
+      Console.WriteLine("'~~~'");  
+      Console.WriteLine("Spooky Ghost");
+
+      /*
+        Try to print the following creature:
+        c_     ___
+        　 \\__|__|__
+        　　 \( o_o)
+        　　　 > ~  >
+        　　　/ 　 / \\
+        　　 /　　/　 \\
+        　　 \　 )　　c/
+        　　/　 /
+        　 /　/|
+        　(　( \
+        　|　|  \
+        　| / \  )
+        　| |　) |
+        /  )(_/
+        (_ /
+      */
+     }
+  }
+}
